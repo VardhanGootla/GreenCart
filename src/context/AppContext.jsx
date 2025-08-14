81d51c7aeddb66935b1a886bf585e3ef4d463d4f
VardhaGootla <your-email@example.com>
2025-08-14 18:24:23 +0530

    Day 1: Added navbar layout

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+
+  const value = { navigate, user, setUser, setIsSeller,isSeller, setShowUserLogin,showUserLogin };
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};

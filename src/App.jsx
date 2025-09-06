268c4231e264c5c7bb2f5dd3bed9e95d24652523
Vardhan Gootla <vardhangootla@gmail.com>
2025-09-06 20:20:36 +0530

    Added Seller Properties

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,28 @@ import Cart from "./pages/Cart.jsx";
 import AddAddress from "./pages/AddAddress.jsx";
 import MyOrders from "./pages/MyOrders.jsx";
 import SellerLogin from "./components/seller/SellerLogin.jsx";
+import SellerLayout from "./pages/seller/SellerLayout.jsx";
+import AddProduct from "./pages/seller/AddProduct.jsx";
+import ProductList from "./pages/seller/ProductList.jsx";
+import Orders from "./pages/seller/Orders.jsx";
 
 const App = () => {
-  const { pathname } = useLocation();
-  const isSellerPath = pathname.includes("seller");
+  const location = useLocation();
+  const isSellerPath = location.pathname.includes("seller");
+
   const containerClasses = isSellerPath
     ? ""
     : "px-6 md:px-16 lg:px-24 xl:px-32";
   const { showUserLogin,isSeller } = useAppContext();
 
   return (
-    <div>
+    <div className="text-default min-h-screen text-gray-700 bg-white">
       {!isSellerPath && <Navbar />}
       {showUserLogin && <Login />}
       <Toaster />
       <div className={containerClasses}>
         <Routes>
+
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<AllProducts />} />
           <Route path="/products/:category" element={<ProductCatagory />} />
@@ -36,9 +42,12 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
-          <Route path="/seller" element={isSeller ? null : <SellerLogin/> }> 
+          <Route path="/seller" element={isSeller ? <SellerLayout /> : <SellerLogin />} />
+         <Route index element={isSeller ? <AddProduct/> : null} />
+         <Route path="product-list" element={<ProductList/>} />
+         <Route path="orders" element={<Orders/>} />
 
-          </Route>
+
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
